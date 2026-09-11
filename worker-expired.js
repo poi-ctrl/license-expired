46d2c5350efad57149e61772d766e4e70959f6ad
poi-ctrl <sonatadanamikadamanik@gmail.com>
2026-09-11 11:27:53 +0700

    Update expiration date for BYTEN user

--- worker-expired.js
+++ worker-expired.js
@@ -26,7 +26,7 @@ const LICENSES = {
   "BYTEN": {
     "password": "byten01",
     "status": "active",
-    "expired": "2026-09-30",
+    "expired": "2026-09-10",
     "device_id": null
   },
   "TOKYO": {
